d98dbd1d270ae66facae18463f5265886d61c5b7
Iván Vallés Pérez <ivanvallesperez@gmail.com>
2020-03-26 17:01:48 +0100

    Fix deprecated argument in gym registration code

--- gym_inventory/__init__.py
+++ gym_inventory/__init__.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 register(
     id='Inventory-v0',
     entry_point='gym_inventory.envs:InventoryEnv',
-    timestep_limit=1000,
+    max_episode_steps=1000,
     reward_threshold=1.0,
     nondeterministic = True,
 )
